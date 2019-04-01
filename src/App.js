47b3ef9124d0f5f7f96fb96b96af819eb3c89715
Pavan Singisetty <Pavan_Singisetty@ad.infosys.com>
2019-04-01 14:48:04 +0530

    Test operation

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Pavan Priyanka Adhrit -- feature branch
+            Pavan Priyanka Adhrit -- Test GIT
           </p>
           <a
             className="App-link"
